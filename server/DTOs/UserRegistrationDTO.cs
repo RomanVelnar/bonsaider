f20803ff89eff22a7860ec20fc36fa1f10678748
Roman Velnar <roman.velnar@gmail.com>
2025-03-03 15:31:35 +0000

    Added password to the URDTO

--- server/DTOs/UserRegistrationDTO.cs
+++ server/DTOs/UserRegistrationDTO.cs
@@ -3,7 +3,8 @@ namespace Server.DTOs
 {
     public class UserRegistrationDTO : CommonDTO
     {
-        public string UserName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
