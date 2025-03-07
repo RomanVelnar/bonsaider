fdb6e5a8c49b2a9b7d20fed436ea25be120fd701
Roman Velnar <roman.velnar@gmail.com>
2025-03-07 22:49:41 +0000

    Added comments to the model

--- server/Models/Bonsai.cs
+++ server/Models/Bonsai.cs
@@ -1,6 +1,28 @@
-public class Bonsai
+using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models
 {
-    public string Name { get; set; }
-    public string Age { get; set; }
-    public string Species { get; set; }
+    public class Bonsai : Common
+    {
+        /// <summary>
+        /// Name of the Bonsai tree 
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+     
+        /// <summary>
+        /// Age of the Bonsai 
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string Age { get; set; }
+        
+        /// <summary>
+        /// Kind of tree species bonsai is 
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string Species { get; set; }
+    }
 }
