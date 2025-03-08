1ddb0bc2d6fbf8bb76d8dd8188134a3f0ca4dbc2
Roman Velnar <roman.velnar@gmail.com>
2025-03-08 21:28:16 +0000

    Removed the commented code

--- server/Endpoints/UserEndpoints.cs
+++ server/Endpoints/UserEndpoints.cs
@@ -17,10 +17,9 @@ namespace Server.Endpoints
         {
             _context = context;
         }
-        
+
         // GET: api/users/{id}
         [HttpGet("{id:guid}")]
-
         public async Task<ActionResult<User>> GetUser(Guid id)
         {
             var user = await _context.Users.FindAsync(id);
@@ -28,11 +27,11 @@ namespace Server.Endpoints
             {
                 return NotFound();
             }
-            
+
             return Ok(user);
         }
-        
-        
+
+
         // POST: api/users
         public async Task<ActionResult<User>> CreateUser([FromBody] UserRegistrationDTO dto)
         {
@@ -59,49 +58,3 @@ namespace Server.Endpoints
         }
     }
 }
-
-
-//
-// namespace Server.Endpoints
-// {
-//     public static class UserEndpoints
-//     {
-//         // Extension method to register user-related endpoints
-//         public static IEndpointRouteBuilder MapUserEndpoints(this IEndpointRouteBuilder app)
-//         {
-//             // POST /users endpoint to register a new user
-//             app.MapPost("/users", async (BonsaiContext db, UserRegistrationDTO dto) =>
-//             {
-//                 // Create a new user using the provided DTO data
-//                 var newUser = new User
-//                 {
-//                     Username = dto.Username,
-//                     Email = dto.Email,
-//                     PasswordHash = HashPassword(dto.Password)
-//                 };
-//
-//                 // Add the new user to the context and save changes
-//                 db.Users.Add(newUser);
-//                 await db.SaveChangesAsync();
-//
-//                 // Return a Created result with a location and some user data
-//                 return Results.Created($"/users/{newUser.Id}", new
-//                 {
-//                     Message = "User created successfully!",
-//                     newUser.Username,
-//                     newUser.Email
-//                 });
-//             });
-//
-//             return app;
-//         }
-//
-//         // Helper method to hash passwords using SHA256
-//         private static string HashPassword(string password)
-//         {
-//             using var sha256 = SHA256.Create();
-//             var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-//             return Convert.ToBase64String(bytes);
-//         }
-//     }
-// }
